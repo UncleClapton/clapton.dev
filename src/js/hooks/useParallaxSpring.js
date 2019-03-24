507c2bbed87b67ef5825cf13c4e8857e6b6a1b74
Cameron Welter <camjwelt@gmail.com>
2019-03-24 02:17:04 +0000

    add custom origin, limit, deadzone, and spring config params.

--- src/js/hooks/useParallaxSpring.js
+++ src/js/hooks/useParallaxSpring.js
@@ -1,34 +1,72 @@
 /* eslint-disable no-magic-numbers */
 import { useSpring } from 'react-spring'
 
-const calcBgPos = (clientX, clientY) => [
-  clientX - (window.innerWidth / 2),
-  clientY - (window.innerHeight / 2),
-]
-
-
 const calcPos = (clientX, clientY) => [
-  Math.max(Math.min(clientX - (window.innerWidth / 2), 800), -800),
-  Math.max(Math.min((clientY - (window.innerHeight / 2)) + (window.innerHeight * 0.1767578125), 400), -400),
+  clientX - (window.innerWidth * 0.5),
+  clientY - (window.innerHeight * 0.5),
 ]
 
-const useParallaxSpring = () => {
+
+const calcAdjPos = (clientX, clientY, origin, limit, deadzone) => {
+  const [xOrigin, yOrigin] = origin
+
+  let pos = [
+    clientX - (window.innerWidth * (xOrigin / 100)),
+    clientY - (window.innerHeight * (yOrigin / 100)),
+  ]
+
+  if (limit && limit.length === 4) {
+    const [topLimit, rightLimit, bottomLimit, leftLimit] = limit
+    pos = [
+      Math.max(Math.min(pos[0], rightLimit), leftLimit),
+      Math.max(Math.min(pos[1], bottomLimit), topLimit),
+    ]
+  }
+
+  if (deadzone && deadzone.length === 4) {
+    const [topDZ, rightDZ, bottomDZ, leftDZ] = deadzone
+
+    if (pos[0] >= leftDZ && pos[0] <= rightDZ && pos[1] >= topDZ && pos[1] <= bottomDZ) {
+      pos = [0, 0]
+    }
+  }
+
+  return pos
+}
+
+const useParallaxSpring = ({
+  origin,
+  deadzone,
+  limit,
+  config,
+}) => {
   const [parallaxSpring, setParallax] = useSpring(() => ({
-    bgxy: [0, 0],
-    xy: [0, 0],
+    adjPos: [0, 0],
+    pos: [0, 0],
     config: {
-      mass: 10,
+      mass: 5,
       tension: 550,
       friction: 140,
+      ...config,
     },
   }))
 
-  const setPos = ({ clientX, clientY }) => setParallax({
-    bgxy: calcBgPos(clientX, clientY),
-    xy: calcPos(clientX, clientY),
+  const handleMouseMove = ({ clientX, clientY }) => setParallax({
+    pos: calcPos(clientX, clientY),
+    ...(origin
+      ? {
+        adjPos: calcAdjPos(
+          clientX,
+          clientY,
+          origin,
+          limit,
+          deadzone
+        ),
+      }
+      : {}),
   })
 
-  return [parallaxSpring, setPos]
+  return [parallaxSpring, handleMouseMove]
 }
 
 
