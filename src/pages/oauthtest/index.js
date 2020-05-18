96ee807bf1a25d91c7af328b3e74fca2542edff4
Cameron Welter <camjwelt@gmail.com>
2020-05-18 20:55:35 +0000

    add JSONTree

--- src/pages/oauthtest/index.js
+++ src/pages/oauthtest/index.js
@@ -2,17 +2,18 @@
 import Head from 'next/head'
 import { useRouter } from 'next/router'
 import React from 'react'
-
+import JSONTree from 'react-json-tree'
 
 // Component Imports
 import OAuthGenerator from '~/components/OAuthGenerator'
 import styles from '~/css/pages/oauthtest.module.css'
+import base16Brewer from '~/themes/base16-brewer'
 
 
 
 
 
-function OAuthTest (ctx) {
+function OAuthTest () {
   const { query } = useRouter()
   const hasQuery = Boolean(Object.keys(query ?? {}).length)
   return (
@@ -21,19 +22,17 @@ function OAuthTest (ctx) {
       <div className={['card', styles.page]}>
         <div className={styles.page}>
           <h1 className={styles.title}>{'Fuel Rats OAuth Tester'}</h1>
-          <span>{'Test for fuelrats.com implicit auth flow'}</span>
           <br />
           {
-            hasQuery && (
-              <pre>
-                {JSON.stringify(query)}
-              </pre>
-            )
-          }
-          {
-            !hasQuery && (
-              <OAuthGenerator />
-            )
+            hasQuery
+              ? (<div className={styles.jsonTree}><JSONTree data={query} invertTheme={false} theme={base16Brewer} /></div>)
+              : (
+                <>
+                  <span>{'Test for fuelrats.com implicit auth flow'}</span>
+                  <br />
+                  <OAuthGenerator />
+                </>
+              )
           }
         </div>
       </div>
